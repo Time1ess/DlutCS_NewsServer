b17dd5deafb07a2315ed816be15c2373dcbb43b7
Time1ess <449195172@qq.com>
2015-10-13 22:06:07 +0800

    主页修复，关于页面添加，单条新闻页面添加

--- news/views.py
+++ news/views.py
@@ -1,7 +1,33 @@
+# -*- coding:gb18030 -*-
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from .models import News
 
 def index(request):
     news=News.objects.all()
-    return render(request,'news/index.html',{'news':news})
+    hot_news=News.objects.all().order_by('-views')[:3]
+    favorite_news=News.objects.all().order_by('-likes')[:3]
+    return render(request,'news/index.html',{
+        'news':news,
+        'hot_news':hot_news,
+        'favorite_news':favorite_news
+        })
+
+def news(request):
+    news_id=request.GET['id']
+    try:
+        news=News.objects.get(id=news_id)
+        return render(request,'news/news.html',{
+            'news':news,
+            })
+    except:
+        return render(request,'news/news.html',{
+            'news':None,
+            })
+
+def about(request):
+    hot_news=News.objects.all().order_by('-views')[:3]
+    favorite_news=News.objects.all().order_by('-likes')[:3]
+    return render(request,'news/about.html',{
+        'hot_news':hot_news,
+        'favorite_news':favorite_news})
